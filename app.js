001c5fa6fd604b2a35b99b09bcdcacbf6ca819a8
Cristian camilo <cristianmilo07@gmail.com>
2023-05-28 20:30:34 -0500

    error subido para corregir

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 8085
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+//const express = require('express')
+//const app = express()
+
+//app.get('/', function (req, res) {
+//  res.send('Hello World')
+//})
+
+//app.listen(3000)
